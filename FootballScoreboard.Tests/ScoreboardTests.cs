a1b36d94e7dccbcde303badafe909599c7b30b4e
abdullah-al-sajid <aasajid200699@gmail.com>
2025-07-08 14:23:47 +0200

    Add failing test for starting a new match

---
+++ FootballScoreboard.Tests/ScoreboardTests.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FootballScoreboard.Tests
+{
+    public class ScoreboardTests
+    {
+        [Test]
+        public void StartMatch_WithHomeAndAwayTeam_ShouldAddMatch()
+        {
+            var scoreboard = new Scoreboard();
+
+            scoreboard.StartMatch("Mexico", "Canada");
+
+            var summary = scoreboard.GetSummary();
+
+            Assert.That(summary.Count, Is.EqualTo(1));
+            Assert.That(summary[0].HomeTeam, Is.EqualTo("Mexico"));
+            Assert.That(summary[0].AwayTeam, Is.EqualTo("Canada"));
+        }
+    }
+}
