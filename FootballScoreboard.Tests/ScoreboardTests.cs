1f589d0ed6453869ef1a66d98ab17357cc3fa9a1
abdullah-al-sajid <aasajid200699@gmail.com>
2025-07-08 16:41:52 +0200

    Added edge testcases for start match

--- FootballScoreboard.Tests/ScoreboardTests.cs
+++ FootballScoreboard.Tests/ScoreboardTests.cs
@@ -3,20 +3,77 @@ namespace FootballScoreboard.Tests
 {
     public class ScoreboardTests
     {
+        private Scoreboard _scoreboard;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _scoreboard = new Scoreboard();
+        }
+
         [Test]
         public void StartMatch_WithHomeAndAwayTeam_ShouldAddMatch()
         {
-            var scoreboard = new Scoreboard();
+            _scoreboard.StartMatch("Mexico", "Canada");
 
-            scoreboard.StartMatch("Mexico", "Canada");
-
-            var summary = scoreboard.GetSummary();
+            var summary = _scoreboard.GetSummary();
 
             Assert.That(summary.Count, Is.EqualTo(1));
             Assert.That(summary[0].HomeTeam, Is.EqualTo("Mexico"));
             Assert.That(summary[0].AwayTeam, Is.EqualTo("Canada"));
             Assert.That(summary[0].HomeScore, Is.EqualTo(0));
             Assert.That(summary[0].AwayScore, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void StartMatch_WithMultipleMatches_ShouldAddAllMatches()
+        {
+            _scoreboard.StartMatch("Mexico", "Canada");
+            _scoreboard.StartMatch("Spain", "Brazil");
+
+            var summary = _scoreboard.GetSummary();
+            Assert.That(summary.Count, Is.EqualTo(2));
+            Assert.That(summary[0].HomeTeam, Is.EqualTo("Mexico"));
+            Assert.That(summary[0].AwayTeam, Is.EqualTo("Canada"));
+            Assert.That(summary[1].HomeTeam, Is.EqualTo("Spain"));
+            Assert.That(summary[1].AwayTeam, Is.EqualTo("Brazil"));
+        }
+
+        [Test]
+        public void StartMatch_WithSameHomeAndAwayTeam_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => _scoreboard.StartMatch("Mexico", "Mexico"));
+        }
+
+        [Test]
+        public void StartMatch_WithDuplicateMatch_ShouldThrowException()
+        {
+            _scoreboard.StartMatch("Mexico", "Canada");
+
+            Assert.Throws<InvalidOperationException>(() => _scoreboard.StartMatch("Mexico", "Canada"));
+        }
+
+        [Test]
+        public void StartMatch_WithDuplicateHomeOrAwayTeam_ShouldThrowException()
+        {
+            _scoreboard.StartMatch("Mexico", "Canada");
+
+            Assert.Throws<InvalidOperationException>(() => _scoreboard.StartMatch("Mexico", "Brazil"));
+        }
+
+        [Test]
+        public void StartMatch_WithNullHomeTeam_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => _scoreboard.StartMatch(null, "Canada"));
+            Assert.Throws<ArgumentException>(() => _scoreboard.StartMatch("Mexico", null));
+
+        }
+
+        [Test]
+        public void StartMatch_WithEmptyAwayTeam_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => _scoreboard.StartMatch("Mexico", ""));
+            Assert.Throws<ArgumentException>(() => _scoreboard.StartMatch("", "Canada"));
         }
     }
 }
